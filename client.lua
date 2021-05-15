f8987e1ee18731e6edc23dd9b75a2d370c9e4eba
MonsieuRGAYA <68069714+gayanoide@users.noreply.github.com>
2021-05-15 03:18:22 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,10 @@
+print("^0======================================================================^7")
+print("^0[^4Author^0] ^7:^0 ^5MonsieuRGAYA^7")
+print("^0[^3Version^0] ^7:^0 ^01.0^7")
+print("^0======================================================================^7")
+
+
+
 --how long you want the thing to last for. in seconds.
 announcestring = false
 lastfor = 15
@@ -35,5 +42,3 @@ Citizen.CreateThread(function()
         end
     end
 end)
-
--------------------- MonsieuRGAYA --------------------------
